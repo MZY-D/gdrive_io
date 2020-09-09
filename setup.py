4eccc62ba756252d3b0fff4f0ed9be1eefbf4a6c
MITA <support@lixnect.com>
2020-09-09 16:57:16 +0900

    version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="gdrive_io",
-	version="0.1",
+	version="0.1.2",
     author="",
     author_email="",
 	description="Easy to input files on GoogleDrive as pandas DataFrame fortmat and output(upload) DataFrames onto GoogleDrive powered by Pandas and Pydrive.",
